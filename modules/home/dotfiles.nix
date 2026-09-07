5d386322bc3366196e819046bdc4dc59a0722c15
WeebNetsu <stephenvdw7777@gmail.com>
2026-09-07 20:32:57 +0200

    Add lua config to symlink and update path to be more flexible

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -7,17 +7,18 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/hypr/hyprland.conf".source =
-      config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/hypr/hyprland.conf;
+    # ".config/hypr/hyprland.conf".source =
+    #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/hypr/hyprland.conf";
+    ".config/hypr/hyprland.lua".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/hypr/hyprland.lua";
     ".config/hypr/hyprpaper.conf".source =
-      config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/hypr/hyprpaper.conf;
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/hypr/hyprpaper.conf";
     ".local/share/nemo/actions/open_xfce_terminal.nemo_action".source =
-      config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/.local/share/nemo/actions/open_xfce_terminal.nemo_action;
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/.local/share/nemo/actions/open_xfce_terminal.nemo_action";
     # ".config/flameshot/flameshot.ini".source =
-    #   config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/flameshot/flameshot.ini;
+    #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/flameshot/flameshot.ini";
     ".config/waybar/config.jsonc".source =
-      config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/waybar/config.jsonc;
-    # ".bashrc".source = config.lib.file.mkOutOfStoreSymlink /home/netsu/Documents/configs/.bashrc;
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/configs/waybar/config.jsonc";
 
     # makes npm i -g work
     ".npmrc".text = ''
