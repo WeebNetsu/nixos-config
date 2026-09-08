e31cf648d9045d57232dba4f870dd310fb39087d
WeebNetsu <stephenvdw7777@gmail.com>
2026-09-08 06:40:47 +0200

    Tmp comment hardware settings

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -8,16 +8,16 @@
 
 {
   # render with GPU not CPU
-  hardware = {
-    graphics.enable = true;
-    # if I wanna start gaming, add below line
-    # graphics.enable32Bit = true;
+  # hardware = {
+  #   graphics.enable = true;
+  # if I wanna start gaming, add below line
+  # graphics.enable32Bit = true;
 
-    nvidia = {
-      modesetting.enable = true;
-      open = true;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-  };
+  #  nvidia = {
+  #    modesetting.enable = true;
+  #    open = true;
+  #    nvidiaSettings = true;
+  #    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #  };
+  # };
 }
